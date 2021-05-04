3f3701d0d51a69b5526626182835ddeafbac1031
adamholcomb <adam.holcomb@ymail.com>
2021-05-04 16:00:08 -0500

    Add election analysis (percent and total vote count)

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,25 +3,23 @@ import os
 import csv
 csvpath = os.path.join("Resources","election_data.csv")
 
-# Create dictionary to store data
+# Create dictionary to count total votes
 polldict = {}
-
 with open(csvpath) as csvfile:
     polldata = csv.reader(csvfile, delimiter=",")
     csv_header = next(polldata)
+    for row in polldata:
+        if row[2] in polldict:
+            polldict[row[2]] += 1
+        else:
+            polldict[row[2]] = 1
+    print("Election Results")
+    print("---------------------------")
+    totalvotes = sum(polldict.values())
+    print(f"Total Votes: {totalvotes}")
+    print("---------------------------")
 
-    # Add data to dictionary
-    polldict = {rows[2]:rows[1] for rows in polldata}
+    for key, value in polldict.items():
+        print(f"{key}: {round(value/totalvotes*100,2)}%, {value} votes")
+    print("---------------------------")
 
-# Print Headers
-print("Election Results")
-print("---------------------------")
-
-# Total Votes
-print(f"Total Votes: {len(polldict)}")
-print("---------------------------")
-
-# List of Vote-Recieving Candidates
-
-for candidate,county in polldict.items():
-    print(candidate)
